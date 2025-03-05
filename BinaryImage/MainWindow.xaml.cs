d760f870b5885b609e0c0ed7bb4feaa9f5d6790f
Дарья <zininadashuta167@gmail.com>
2025-03-05 20:35:54 +0300

    Почистила код, приложение готово.

--- BinaryImage/MainWindow.xaml.cs
+++ BinaryImage/MainWindow.xaml.cs
@@ -16,38 +16,29 @@ namespace BinaryImage
         {
             InitializeComponent();
         }
-
         private void SelectImage_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog
             {
                 Filter = "Изображения (*.png;*.jpg;*.jpeg;*.bmp)|*.png;*.jpg;*.jpeg;*.bmp"
             };
-
             if (openFileDialog.ShowDialog() == true)
             {
-                // Отображаем изображение
                 SelectedImage.Source = new BitmapImage(new Uri(openFileDialog.FileName));
-
-                // Конвертируем в бинарный код
                 string binaryData = ConvertImageToBinary(openFileDialog.FileName);
                 BinaryOutput.Text = binaryData;
             }
         }
-
         private string ConvertImageToBinary(string imagePath)
         {
             byte[] imageBytes = File.ReadAllBytes(imagePath);
             StringBuilder binaryString = new StringBuilder();
-
             foreach (byte b in imageBytes)
             {
                 binaryString.Append(Convert.ToString(b, 2).PadLeft(8, '0')).Append(" ");
             }
-
             return binaryString.ToString();
         }
-
         private void CopyBinary_Click(object sender, RoutedEventArgs e)
         {
             if (!string.IsNullOrEmpty(BinaryOutput.Text))
